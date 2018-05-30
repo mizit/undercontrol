e3c446d73184dfa1d993758dc1498b318758a49b
mizit <luden16@yandex.ru>
2018-05-30 23:41:16 +0300

    DnD GUI

--- Under Control/Assets/SystemParent.cs
+++ Under Control/Assets/SystemParent.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class BaseSlider
 {
     public BaseSlider()
@@ -59,10 +61,208 @@ public class BaseSlider
     }
 }
 
-public class Event
+/*public class Event
 {
     private MonoBehaviour goal;
     private 
+}*/
+
+public class GUIElement
+{
+    private Rect rect;
+    private Point position;
+    public Rect Rect
+    {
+        get
+        {
+            return rect;
+        }
+
+        set
+        {
+            rect = value;
+            position.x = rect.x;
+            position.y = rect.y;
+        }
+    }
+    public Point Position
+    {
+        get
+        {
+            return position;
+        }
+
+        set
+        {
+            position = value;
+            rect.x = position.x;
+            rect.y = position.y;
+        }
+    }
+
+    private bool dnd;
+    private bool dnd_allowed;
+    private bool pressed;
+    private Point dnd_offset;
+
+    private bool DnD
+    {
+        get
+        {
+            return dnd;
+        }
+
+        set
+        {
+            if (DnD_allowed)
+            {
+                dnd = value;
+            }
+            else
+            {
+                dnd = false;
+            }
+        }
+    }
+    public bool DnD_allowed
+    {
+        get
+        {
+            return dnd_allowed;
+        }
+
+        set
+        {
+            dnd_allowed = value;
+            if (dnd_allowed == false)
+            {
+                DnD = false;
+            }
+        }
+    }
+
+    private bool IsMouseOn
+    {
+        get
+        {
+            if (GraphMath.IsPointInRect(new Point(Mouse.Position), Rect))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    private bool IsPressed
+    {
+        get
+        {
+            if (IsMouseOn && Input.GetMouseButtonDown(0))
+            {
+                pressed = true;
+                return true;
+            }
+            return false;
+        }
+    }
+    private bool IsReleased
+    {
+        get
+        {
+            if (pressed && Input.GetMouseButtonUp(0))
+            {
+                pressed = false;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public GUIElement()
+    {
+        Rect = new Rect();
+        Position = new Point();
+    }
+
+    private Point Dnd_offset
+    {
+        get
+        {
+            return dnd_offset;
+        }
+
+        set
+        {
+            dnd_offset = value;
+        }
+    }
+
+    public void Processing()
+    {
+        if (DnD_allowed)
+        {
+            if (IsPressed)
+            {
+                DnD = true;
+                Dnd_offset = Position - (new Point(Mouse.Position));
+            }
+            if (IsReleased)
+            {
+                DnD = false;
+            }
+        }
+        if (DnD)
+        {
+            Position = (new Point(Mouse.Position)) + Dnd_offset;
+        }
+    }
+}
+
+public class SystemDraw : GUIElement
+{
+    private Texture2D back;
+    private Texture2D slider;
+    private string name;
+    
+
+    public Texture2D Back
+    {
+        get
+        {
+            return back;
+        }
+
+        set
+        {
+            back = value;
+        }
+    }
+    public Texture2D Slider
+    {
+        get
+        {
+            return slider;
+        }
+
+        set
+        {
+            slider = value;
+        }
+    }
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+    
+
+
 }
 
 public class SystemParent : MonoBehaviour
@@ -70,6 +270,7 @@ public class SystemParent : MonoBehaviour
     public BaseSlider Strength;
     protected float fear;
     protected float brokennes;
+    protected SystemDraw sys_draw;
 
     public float Fear       //ползунок страха ограниченный, максимум и минимумом силы
     {
@@ -98,6 +299,10 @@ public class SystemParent : MonoBehaviour
     void Start()
     {
         Strength = new BaseSlider();
+        sys_draw = new SystemDraw();
+        sys_draw.DnD_allowed = true;
+        sys_draw.Rect = new Rect(0, 0, 100, 50);
+        sys_draw.Name = "One";
     }
 
     // Update is called once per frame
@@ -113,8 +318,14 @@ public class SystemParent : MonoBehaviour
             Strength.Value -= 10;
             Debug.Log("Current strength - " + Strength.Value);
         }
+        sys_draw.Processing();
     }
 
+
+    void OnGUI()
+    {
+        GUI.Box(sys_draw.Rect, Mouse.Position.x.ToString() + " " + Mouse.Position.y.ToString());//sys_draw.Name);
+    }
     //Update is called once per turn
     void Turn()
     {
---
+++ Under Control/Assets/Utility.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Point
+{
+    public float x;
+    public float y;
+    public Point (float X, float Y)
+    {
+        x = X;
+        y = Y;
+    }
+    public Point (Vector2 vec)
+    {
+        x = vec.x;
+        y = vec.y;
+    }
+    public Point (Vector3 vec)
+    {
+        x = vec.x;
+        y = vec.y;
+    }
+    public Point (Rect rec)
+    {
+        x = rec.x;
+        y = rec.y;
+    }
+    public Point (Point p)
+    {
+        x = p.x;
+        y = p.y;
+    }
+    public static Point operator+ (Point p1, Point p2)
+    {
+        return new Point(p1.x + p2.x, p1.y + p2.y);
+    }
+    public static Point operator- (Point p1, Point p2)
+    {
+        return new Point(p1.x - p2.x, p1.y - p2.y);
+    }
+}
+
+public class GraphMath
+{
+    public static bool IsPointInRect(Point p, Rect r)
+    {
+        if ((p.x > r.x) && (p.x < r.x + r.width) &&
+            (p.y > r.y) && (p.y < r.y + r.height))
+        {
+            return true;
+        }
+        //Debug.Log(p.x.ToString() + " " + p.y.ToString() + " " + r.x.ToString() + " " + r.y.ToString() + " " + r.width.ToString() + " " + r.height.ToString());
+        return false;
+    }
+    /*public static Point SetPointInRect(Point p, Rect r)
+    {
+        Point ans = new Point(p);
+        ans.x = Mathf.Max(Mathf.Min(p.x, r.width), 0);
+        ans.y = Mathf.Max(Mathf.Min(p.y, r.height), 0);
+        return ans;
+    }*/
+}
+
+public class Mouse
+{
+    public static Point Position
+    {
+        get
+        {
+            return new Point(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+        }
+    }
+}
